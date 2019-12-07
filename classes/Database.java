5fd91a60396a46e24d24848de8c7c320776d4b7f
caiomartins1 <45666995+caiomartins1@users.noreply.github.com>
2019-12-07 00:34:32 +0000

    Update Database.java

--- classes/Database.java
+++ classes/Database.java
@@ -1,4 +1,4 @@
- package poo;
+package poo;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -49,26 +49,62 @@ public class Database {
     }
 
 
-    public static ArrayList<Artista> adicionarArtista(ArrayList<Artista> listaArtistas, Artista a) throws artistaDuplicadoException {
+    public static ArrayList<Artista> adicionarArtista(ArrayList<Artista> listaArtistas, Artista a) throws ArtistaDuplicadoException {
         for (int i = 0; i < listaArtistas.size(); i++) {
             if (listaArtistas.get(i).getId() == a.getId()) {
-                throw new artistaDuplicadoException("O artista que tentou adicionar ja existe!!");
+                throw new ArtistaDuplicadoException("O artista que tentou adicionar ja existe!!");
             }
         }
         listaArtistas.add(a);
         return listaArtistas;
     }
 
-    public static Artista acharArtistaPorID(ArrayList<Artista> listaArtistas, int id) throws artistaNaoEncontrado {
+    public static Artista acharArtistaPorID(ArrayList<Artista> listaArtistas, int id) throws ArtistaNaoEncontradoException {
         for (int i = 0; i < listaArtistas.size(); i++) {
             if(listaArtistas.get(i).getId() == id) {
                 return listaArtistas.get(i);
             }
         }
-        throw new artistaNaoEncontrado("O artista que procura nao existe!");
+        throw new ArtistaNaoEncontradoException("O artista que procura nao existe!");
     }
 
+    public static ArrayList<Artista> removerArtistaID(ArrayList<Artista> listaArtistas, Artista a) throws ArtistaNaoEncontradoException {
 
+        if (listaArtistas.indexOf(a) != -1) {
+            listaArtistas.remove(listaArtistas.indexOf(a));
+            return listaArtistas;
+        } else {
+            throw new ArtistaNaoEncontradoException("O artista que esta tentando remover nao existe!");
+        }
 
+    }
 
+    public static ArrayList<Espetaculo> adicionarEspetaculo(ArrayList<Espetaculo> listaEspetaculos, Espetaculo e) throws EspetaculoDuplicadoException {
+        for (int i = 0; i < listaEspetaculos.size(); i++) {
+            if (listaEspetaculos.get(i).getId() == e.getId()) {
+                throw new EspetaculoDuplicadoException("O espetaculo que tentou adicionar ja existe!!");
+            }
+        }
+        listaEspetaculos.add(e);
+        return listaEspetaculos;
+    }
+
+    public static Espetaculo acharEspetaculoPorID(ArrayList<Espetaculo> listaEspetaculos, int id) throws EspetaculoNaoEncontradoException {
+        for (int i = 0; i < listaEspetaculos.size(); i++) {
+            if(listaEspetaculos.get(i).getId() == id) {
+                return listaEspetaculos.get(i);
+            }
+        }
+        throw new EspetaculoNaoEncontradoException("O espetaculo que procura nao existe!");
+    }
+
+    public static ArrayList<Espetaculo> removerEspetaculoID(ArrayList<Espetaculo> listaEspetaculos, Espetaculo a) throws EspetaculoNaoEncontradoException {
+
+        if (listaEspetaculos.indexOf(a) != -1) {
+            listaEspetaculos.remove(listaEspetaculos.indexOf(a));
+            return listaEspetaculos;
+        } else {
+            throw new EspetaculoNaoEncontradoException("O espetaculo que esta tentando remover nao existe!");
+        }
+    }
 }
