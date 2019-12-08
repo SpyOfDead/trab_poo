f5e5e67cca4b3ca3644699cee915c6e7a0a02c26
caiomartins1 <45666995+caiomartins1@users.noreply.github.com>
2019-12-08 15:17:06 +0000

    Update mainModificado.java

--- poo/mainModificado.java
+++ poo/mainModificado.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 
-public class testebesta {
+public class Main {
     public static void main(String[] args) {
 
         ArrayList<Espetaculo> listaEspetaculo = new ArrayList<Espetaculo>();
